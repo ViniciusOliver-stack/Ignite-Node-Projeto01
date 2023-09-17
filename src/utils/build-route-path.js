4814bc9c41df47dbf6a195c1d2566a4ad3b364fc
ViniciusOliver-stack <vinicius.so.contato@hotmail.com>
2023-09-17 13:56:18 -0300

    :sparkles: feat: routes parametersRegex

--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -1,5 +1,14 @@
 export function buildRoutePath(path) {
   const routeParametersRegEx = /:([a-zA-Z]+)/g;
 
+  const pathWithParams = path.replaceAll(
+    routeParametersRegEx,
+    "(?<$1>[a-z0-9-_]+)"
+  );
+
+  const pathRegex = new RegExp(`^${pathWithParams}`);
+
+  return pathRegex;
+
   console.log(Array.from(path.matchAll(routeParametersRegEx)));
 }
