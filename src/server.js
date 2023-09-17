b1d5422467c11b056b681cb4368d9840123c58df
ViniciusOliver-stack <vinicius.so.contato@hotmail.com>
2023-09-17 11:40:54 -0300

    :sparkles:	feat: server

---
+++ src/server.js
@@ -0,0 +1,21 @@
+import http from "node:http";
+import { routes } from "./routes.js";
+import { json } from "./middlewares/json.js";
+
+const server = http.createServer(async (request, response) => {
+  const { method, url } = request;
+
+  await json(request, response);
+
+  const route = routes.find((route) => {
+    return route.method === method && route.path === url;
+  });
+
+  if (route) {
+    return route.handler(request, response);
+  }
+
+  return response.writeHead(404).end("Not Found");
+});
+
+server.listen(3333);
